7f53cfc9e2a9197c290f365fc11bf7ee1ba8facc
jjw5340 <jesse.wengel@proton.me>
2026-07-01 19:35:25 -0400

    Add updatable and drawable pygame groups (CH3L5)

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -28,8 +33,9 @@ def main():
 
         screen.fill("black")
 
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
 
         pygame.display.flip()
 
